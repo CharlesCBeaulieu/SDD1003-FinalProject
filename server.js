59820694428c65a913bb264d2967bdae71a5b652
Raphaël Céré <awaarkel@gmail.com>
2022-12-07 20:12:48 -0500

    Template

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const { json } = require('express');
 const express = require('express');
 const app = express();
+const multiparty = require('multiparty');
+
 const {MongoClient} = require('mongodb');
 var uri = 'mongodb+srv://charlesbeaulieu:DE1h6fOiPa764hmJ@cluster0.ptmtvxt.mongodb.net/?retryWrites=true&w=majority'
 const client = new MongoClient(uri);
@@ -11,14 +12,11 @@ app.get('/', (req, res) => {
     res.render('index')
 })
 
-app.post('/', (req, res) => {
-// Connect to the db
-   
-    //run()
-    console.log(req.body)
-    console.log(req.params)
-    //res.send(JSON.stringify(data))
-    res.send("hiii")
+app.post('/', async (req, res) => {
+  let form = new multiparty.Form();
+  form.parse(req,  async function (err, fields, files) {
+    res.send(JSON.stringify(await run(fields.bedroom[0], fields.minNight[0], fields.maxNight[0])))
+  })
 })
 
 app.listen(process.env.PORT || 5000)
@@ -27,15 +25,10 @@ async function run(bedroom, minNight, maxNight) {
     try {
         const database = client.db('sample_airbnb');
         const listingsAndReviews = database.collection('listingsAndReviews');
-
-        // Query for a movie that has the title 'Back to the Future'
-        const query = {bedrooms : bedroom, minimum_nights : minNight, maximum_nights: maxNight};
-        const listingsAndReview = await listingsAndReviews.find(query);
-
-    } finally {
-        // Ensures that the client will close when you finish/error
-        await client.close();
+        const query = {bedrooms: parseInt(bedroom), minimum_nights: minNight, maximum_nights: maxNight};
+        return listingsAndReviews.find(query).toArray();
+    }catch (e) {
+      await client.close();
     }
 
-    return listingsAndReview
 }
