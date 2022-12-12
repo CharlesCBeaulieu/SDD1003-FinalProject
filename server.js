77e55081e6a3982821feaec139568ef596a5bc71
Charles Beaulieu <78936236+CharlesCBeaulieu@users.noreply.github.com>
2022-12-11 21:59:54 -0500

    ajout

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ var uri = 'mongodb+srv://charlesbeaulieu:DE1h6fOiPa764hmJ@cluster0.ptmtvxt.mongo
 const client = new MongoClient(uri);
 
 //listen port
-app.listen(process.env.PORT || 5000)
+const port = 8000
+app.listen(process.env.PORT || port)
+console.log("Listening to port " + port);
 
 //set expressJS view engine
 app.set('view engine', 'ejs')
@@ -37,14 +39,13 @@ app.get('/read', (req, res) => {
   res.render('index')
 })
 
-//POST method route
+//Read method
 app.post('/', async (req, res) => {
   let form = new multiparty.Form();
   form.parse(req, async function (err, fields, files) {
     res.send(JSON.stringify(await run(fields.bedroom[0], fields.minNight[0], fields.maxNight[0])))
   })
 })
-
 //function to query Bedrooms, minrooms, maxrooms
 async function run(bedroom, minNight, maxNight) {
   try {
@@ -58,5 +59,65 @@ async function run(bedroom, minNight, maxNight) {
   } catch (e) {
     await client.close();
   }
+}
 
+
+
+//delete method
+app.post('/delete', async (req, res) => {
+  let form = new multiparty.Form();
+  form.parse(req, async function (err, fields, files) {
+    res.send(JSON.stringify(await delete_doc(fields.id[0])))
+  })
+})
+//delete fonction with query
+async function delete_doc(id) {
+  try {
+    //access database collection
+    const database = client.db('sample_airbnb');
+    const listingsAndReviews = database.collection('listingsAndReviews');
+
+    //return query result
+    console.log(id)
+    let query = {_id : id }
+    console.log(query)
+    let result = listingsAndReviews.find(query).limit(5).toArray();
+    listingsAndReviews.deleteOne(query);
+    return result;
+  } catch (e) {
+    await client.close();
+  }
+}
+
+//update method
+app.post('/update', async (req, res) => {
+  let form = new multiparty.Form();
+  form.parse(req, async function (err, fields, files) {
+    res.send(JSON.stringify(await update_doc(fields.id[0], fields.field_to_update[0], fields.new_value[0])))
+  })
+})
+//delete fonction with query
+async function update_doc(id, field_to_update, new_value) {
+  try {
+    //access database collection
+    const database = client.db('sample_airbnb');
+    const listingsAndReviews = database.collection('listingsAndReviews');
+
+    //return query result
+    console.log(id)
+    console.log(field_to_update)
+    console.log(new_value)
+    let filter = {_id : id }
+    let update = {field_to_update : new_value}
+    let query = {filter, update}
+    console.log(query)
+
+    let result = listingsAndReviews.find(filter).toArray();
+    listingsAndReviews.updateOne(query);
+    result.push(listingsAndReviews.find(filter).toArray());
+    
+    return result;
+  } catch (e) {
+    await client.close();
+  }
 }
